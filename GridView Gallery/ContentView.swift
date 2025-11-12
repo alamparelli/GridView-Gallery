61bd85643cc4c51e43038036bdda6943b9b2ae69
Alessandro <5610813+alamparelli@users.noreply.github.com>
2025-11-12 14:10:35 +0100

    Initial Commit

---
+++ GridView Gallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2025. Created by Alessandro L. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GridView Gallery/GridView_GalleryApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2025. Created by Alessandro L. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct GridView_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
